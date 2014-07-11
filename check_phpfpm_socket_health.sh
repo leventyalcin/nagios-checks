d1190e6c53eadab6ec3e8641c68d32ff5df5b474
Levent YALCIN <leventyalcin@gmail.com>
2014-07-12 00:21:38 +0300

    checks php-fpm sockets health

---
+++ check_phpfpm_socket_health.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+#title              : check_phpfpm_socket_health.sh
+#author             : Levent Yalcin <leventyalcin [a] gmail com>
+#date               : 20140711
+#description        : Nagios check for php-fpm sockets
+
+POOLD="$1"
+: ${POOLD:="/etc/php5/fpm/pool.d"}
+
+ERR_SOCKET=""
+SOCKETS=0
+
+for i in $(ls -1 ${POOLD}/*.conf 2>/dev/null); do
+    SOCKET_FILE=$(grep -E "^listen.[ \t].=*" "${i}" | awk '{print $NF}')
+    if  [ -n "${SOCKET_FILE}" ] && \
+        [ $(echo /dev/null | socat UNIX:$SOCKET_FILE - 2>/dev/null) ]
+    then
+        ERR_SOCKET="${ERR_SOCKET} ${i}"
+    fi
+    [ -n "${SOCKET_FILE}" ] && let SOCKETS+=1
+done
+
+if [ -n "$ERR_SOCKET" ]; then 
+    echo "CRITICAL - Failed socket(s) : ${i}"
+    exit 2
+elif [ $SOCKETS -eq 0 ]; then
+    echo "WARNING - There is no defined socket under ${POOLD}"
+    exit 1
+else
+    echo "OK - All (${SOCKETS}) sockets seem happy"
+    exit 0
+fi
